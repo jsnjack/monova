3799e1071c6fb02058063b380224262fdbbc02cf
Yauhen Shulitski <jsnjack@gmail.com>
2017-08-27 00:46:52 +0200

    :patch: Add info tag

--- dahary.go
+++ dahary.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	var oldVersion string
+
+	infoFlag := flag.Bool("info", false, "Print old and new version")
+	flag.Parse()
+
 	path, _ := os.Getwd()
 	repo, err := CreateRepository(path)
 	if err != nil {
 		panic(err)
 	}
+
+	if *infoFlag {
+		oldVersion = repo.GetVersion()
+	}
+
 	newVersion, err := repo.UpdateVersion()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(newVersion)
+
+	if *infoFlag {
+		fmt.Printf("%s -> %s\n", oldVersion, newVersion)
+	} else {
+		fmt.Println(newVersion)
+	}
 }
